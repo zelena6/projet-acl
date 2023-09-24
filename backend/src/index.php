768c55be19dfe38d1963822eadff712149b020bd
Phil1010 <philliptysoun@gmail.com>
2023-09-24 20:15:47 +0200

    Set up frontend
    Le frontend peut fetch les données du backend.

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,4 +1,8 @@
 <?php
+// Headers pour autoriser les requêtes cross-origin (CORS)
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -6,11 +10,14 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Crée une instance de l'application Slim
 $app = AppFactory::create();
 
+// Définit une route pour la racine ("/")
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Je suis ton serveur ! l'index.php");
     return $response;
 });
 
+// Lance l'application Slim
 $app->run();
---
+++ frontend/vue-project/src/App.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+
+// Utilisez une référence pour stocker la réponse du serveur
+const serverResponse = ref('');
+
+fetch('http://localhost:8888')
+  // si connexion établie alors message de confirmation
+  .then((response) => {
+    if (response.ok) {
+      // Si la connexion est établie avec succès, lisez le corps de la réponse
+      return response.text();
+    } else {
+      throw new Error('Erreur de connexion !');
+    }
+  })
+  .then((data) => {
+    // Mettez à jour la référence avec le texte de la réponse
+    serverResponse.value = data;
+    console.log('Connexion établie !');
+  })
+  // si connexion non établi alors message d'erreur
+  .catch((error) => {
+    console.error('Erreur de connexion !');
+    console.error(error);
+  });
+</script>
+
+<template>
+  <div>{{ serverResponse }}</div>
+  <br />
+
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <!-- Affichez le texte de la réponse du serveur dans un élément div -->
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
