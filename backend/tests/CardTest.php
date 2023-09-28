eb8ed2d63c3d1b5910038e694fb6e3acadc32244
Nicolas ROBERT <rbtnicolas57@gmail.com>
2023-09-28 16:42:32 +0200

    la couleur de la carte est déduite de la forme

--- backend/tests/CardTest.php
+++ backend/tests/CardTest.php
@@ -6,8 +6,23 @@ use PHPUnit\Framework\TestCase;
 
 final class CardTest extends TestCase
 {
-    public function testCardToJson(): void
+    public function testHeartsCardToJson(): void
     {
-        $this->assertSame('{"color":"black","shape":"clubs","value":"two"}', json_encode(new Card(Color::Black, Shape::Clubs, Value::Two)));
+        $this->assertSame('{"color":"red","shape":"hearts","value":"two"}', json_encode(new Card(Shape::Hearts, Value::Two)));
+    }
+
+    public function testDiamondsCardToJson(): void
+    {
+        $this->assertSame('{"color":"black","shape":"diamonds","value":"two"}', json_encode(new Card(Shape::Diamonds, Value::Two)));
+    }
+
+    public function testClubsCardToJson(): void
+    {
+        $this->assertSame('{"color":"red","shape":"clubs","value":"two"}', json_encode(new Card(Shape::Clubs, Value::Two)));
+    }
+
+    public function testSpadesCardToJson(): void
+    {
+        $this->assertSame('{"color":"black","shape":"spades","value":"two"}', json_encode(new Card(Shape::Spades, Value::Two)));
     }
 }
