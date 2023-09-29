978f9b76837a5d15e8203d5ccd9b435abd08f772
Nicolas ROBERT <rbtnicolas57@gmail.com>
2023-09-29 13:24:38 +0200

    ajout mélange de cartes

--- backend/src/Deck.php
+++ backend/src/Deck.php
@@ -1,13 +1,19 @@
 <?php
 
+/**
+ * Représente un paquet de cartes.
+ */
 class Deck
 {
+    /** @var array Tableau de cartes dans le paquet. */
     private array $cards;
 
+    /**
+     * Constructeur de la classe Deck.
+     */
     function __construct()
     {
         $this->cards = [];
-
         foreach (Shape::cases() as $shape) {
             foreach (Value::cases() as $value) {
                 array_push($this->cards, new Card($shape, $value));
@@ -15,13 +21,34 @@ class Deck
         }
     }
 
+    /**
+     * Obtient les cartes du paquet.
+     *
+     * @return array Le tableau de cartes dans le paquet.
+     */
     public function getCards(): array
     {
         return $this->cards;
     }
 
+    /**
+     * Définit les cartes du paquet.
+     *
+     * @param array $cards Le tableau de cartes à définir dans le paquet.
+     */
     public function setCards(array $cards): void
     {
         $this->cards = $cards;
+    }
+
+    /**
+     * Mélange les cartes dans le paquet.
+     */
+    public function shuffle(): void
+    {
+        for ($i = 0; $i < sizeof($this->cards); $i++) {
+            $r = rand(0, sizeof($this->cards) - 1);
+            [$this->cards[$i], $this->cards[$r]] = [$this->cards[$r], $this->cards[$i]];
+        }
     }
 }
